51d5d7f93d16067f7159ac431ec82f711896b8ab
Claudio Nogueira <cllaudionoggueira@gmail.com>
2020-12-28 01:18:31 -0300

    First commit

---
+++ Generics02/Entities/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Entities {
+    class Product : IComparable{
+        public string _name { get; set; }
+        public double _price { get; set; }
+
+        public Product(string name, double price) {
+            this._name = name;
+            this._price = price;
+        }
+
+        public override string ToString() {
+            return _name + ", " + _price.ToString("F2");
+        }
+
+        public int CompareTo(object obj) {
+            if(!(obj is Product)) {
+                throw new ArgumentException("Comparing error argument is not a product");
+            }
+            Product product = obj as Product;
+            return _price.CompareTo(product._price);
+        }
+    }
+}
